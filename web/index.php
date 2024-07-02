6105eccef4649a8973e6af899b77e529f1d7cb34
unknown <wira.prayoga@student.undiksha.ac.id>
2024-07-02 22:06:43 +0800

    first commit

---
+++ web/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docker</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f8ff;
+            color: #333;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .container {
+            background-color: #001f3f;
+            padding: 40px;
+            border-radius: 15px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+            color: white;
+            width: 80%;
+            max-width: 800px; 
+            height: auto;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 10px;
+            text-align: left;
+            border-bottom: 1px solid #ffffff;
+        }
+        th {
+            font-size: 24px;
+            font-weight: bold;
+        }
+        td {
+            font-size: 18px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <table>
+            <tr>
+                <th colspan="2">Data Mahasiswa</th>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td>Made Wira Prayoga</td>
+            </tr>
+            <tr>
+                <td>Nim</td>
+                <td>2215101005</td>
+            </tr>
+            <tr>
+                <td>Kelas</td>
+                <td>ILKOM-A</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
